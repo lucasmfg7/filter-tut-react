19de3d19ac3372e61c8479c8fd507f1d155a5793
Lucas <lucas_matheus22@hotmail.com>
2022-06-12 10:13:47 -0300

    React Search Multi Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
+import Table from "./Table";
 
 import { Users } from "./users";
 
 const App = () => {
   const [query, setQuery] = useState("");
+
+  const keys = ["first_name", "last_name", "email"];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query.toLowerCase()))
+    );
+  };
 
   return (
     <div className="app">
@@ -14,7 +23,9 @@ const App = () => {
         placeholder="Search"
         onChange={(e) => setQuery(e.target.value)}
       />
-      <ul className="list">
+      <Table data={search(Users)} />
+
+      {/* <ul className="list">
         {Users.filter((user) =>
           user.first_name.toLowerCase().includes(query)
         ).map((user) => (
@@ -22,7 +33,7 @@ const App = () => {
             {user.first_name}
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
