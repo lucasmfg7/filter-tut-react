d5904f3d3192abc93ad57d4086efa68ad823d02e
Lucas <lucas_matheus22@hotmail.com>
2022-06-12 10:26:02 -0300

    API Init

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+import { Users } from "./users.js";
+import cors from "cors";
+
+app.use(cors());
+
+app.get("/", (req, res) => {});
+
+app.listen(5000, () => console.log("API is working!"));
