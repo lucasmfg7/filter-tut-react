333305f23b40086dc494127e43a4a8e996b42154
Lucas <lucas_matheus22@hotmail.com>
2022-06-12 09:54:58 -0300

    React Basic Search Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+import { Users } from "./users";
+
 const App = () => {
-  return <div>App</div>;
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="app">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <ul className="list">
+        {Users.filter((user) =>
+          user.first_name.toLowerCase().includes(query)
+        ).map((user) => (
+          <li key={user.id} className="listItem">
+            {user.first_name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
